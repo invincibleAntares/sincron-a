0302a9777a8ef326f14d8f68aa23731852fc9424
invincibleAntares <kumarabhishek2928@gmail.com>
2025-01-16 00:46:40 +0530

    toaster added sucessfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import EditorPage from "./pages/EditorPage"
+import { Toaster } from "react-hot-toast"
 
 function App() {
 
 
   return (
    <>
-    
+     <div>
+            <Toaster position="top-right"
+              toastOptions={{
+                success:{
+                  theme:{
+                    primary:'#4aed88',
+                  }
+                }
+              }}
+             />
+            
+     </div>
      <BrowserRouter>
          <Routes>
            <Route path="/" element={<HomePage/>} />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
 import { ListFilter } from 'lucide-react';
-import React from 'react';
-
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import { v4 as uuidv4 } from 'uuid';
 function HomePage() {
+
+      const [roomId, setRoomId] = useState('');
+      const [username, setUsername] = useState('');
+       
+      const createNewRoom =(e)=>{
+          e.preventDefault();   
+          const id = uuidv4();
+          setRoomId(id);
+          // console.log(id);
+          toast.success('Room created successfully');
+      }
+     
   return (
     <div className="bg-black min-h-screen flex justify-center items-center">
       <form className="bg-gray-800 p-8 rounded-lg shadow-lg space-y-6 w-full max-w-md">
@@ -17,23 +30,35 @@ function HomePage() {
             type="text"
             placeholder="Room ID"
             className="w-full px-4 py-2 rounded-md bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              onChange={(e) => setRoomId(e.target.value)}
+            value={roomId}
+          
           />
           <input
             type="text"
             placeholder="Username"
             className="w-full px-4 py-2 rounded-md bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            
           />
         </div>
-
+              
         <button
           type="submit"
-          className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-md transition duration-200"
+          className="w-full bg-blue-600 hover:bg-blue-800 text-white font-semibold py-2 px-4 rounded-md transition duration-200"
         >
           Join Room
         </button>
 
         <div className="text-center text-gray-400">
-          <span>If you don't have an invite, create one!</span>
+          <span>If you don't have an invite
+              <button   
+               onClick={createNewRoom}
+              className='mx-3 font-2xl text-white hover:text-blue-500'>
+              create one!
+              </button>
+          </span>
         </div>
       </form>
     </div>
