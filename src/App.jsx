6fde40c79572fd17db3f8d6ed020f30c2b8aed21
invincibleAntares <kumarabhishek2928@gmail.com>
2025-01-15 19:36:02 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import EditorPage from "./pages/EditorPage"
 
 function App() {
 
 
   return (
    <>
-   
-       
+    
+     <BrowserRouter>
+         <Routes>
+           <Route path="/" element={<HomePage/>} />
+           <Route path="/editor/:roomId" element={<EditorPage/>} />
+         </Routes>
+     </BrowserRouter>  
    
    </>
   )
